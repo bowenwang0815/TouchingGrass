062a214cc86ac65042d180f26b6cdd28e44aacfc
Bowen Wang <bowensw@uci.edu>
2024-04-20 18:14:33 -0700

    Initial Commit

---
+++ lahackapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lahackapp
+//
+//  Created by Bowen Wang on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lahackapp/lahackappApp.swift
@@ -0,0 +1,17 @@
+//
+//  lahackappApp.swift
+//  lahackapp
+//
+//  Created by Bowen Wang on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct lahackappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
